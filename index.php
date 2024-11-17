816a07e7e9b8a3ebb8099923762dd47a712a6c14
Santy0-0 <santidomagsa@gmail.com>
2024-11-17 13:16:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mercado Virtual</title>
-    <link rel="stylesheet" href="css/style.css"> <!-- Enlace al archivo CSS externo -->
+    <link rel="stylesheet" href="css/style.css"> <!-- Enlace al archivo CSS externo 222222222222222222222222222222222222-->
 </head>
 <body>
     <div class="header">
